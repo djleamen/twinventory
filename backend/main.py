90f3cd91209a9bb5ca38fd79d7c2af1843c40a38
laygofiona <laygo.fiona@gmail.com>
2026-09-19 12:09:32 -0400

    Adding PersonC stuff

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,13 @@
-def main():
-    print("Hello from backend!")
+# main.py
+from fastapi import FastAPI
+
+app = FastAPI()
+
+# Run uvicorn main:app --reload to run
+
+@app.get("/")
+def read_root():
+    print("FastAPI is running via uvicorn!")
+    return {"status": "success", "message": "FastAPI running via uv!"}
 
 
-if __name__ == "__main__":
-    main()
